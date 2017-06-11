4b10b073a1823a1683f49a0b1734a49019bd0929
Jason <jason@home>
2017-06-11 14:53:55 -0700

    Initial comment

---
+++ RemoveTextFromFile/FileManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveTextFromFile
+{
+    public class FileManager
+    {
+        // File Rename Method
+        public bool renameFile(string sourceFolder, string textToRemove, string textToReplace)
+        {
+            bool result = false;
+            string logName = Path.Combine(sourceFolder, DateTime.Now.ToString("yyyy-MM-dd") + "-rename.log");
+
+            try
+            {
+                string[] files = Directory.GetFiles(sourceFolder, "*", SearchOption.AllDirectories);
+
+                // Gather a list of all files within the source folder
+                foreach(string file in files)
+                {
+
+                    string newFileName = Path.GetFileName(file).Replace(textToRemove, textToReplace);
+                    string newDestination = Path.Combine(Path.GetDirectoryName(file), newFileName);
+                    // Move file if it doesn't exist
+                    if(!File.Exists(newDestination))
+                    {
+                        // Remove unwanted text from file and move it to destination folder
+                        File.Move(file, newDestination);
+
+                        // Log renamed file to source directory
+                        logRenamedFile(logName, file + " -> " + newDestination);
+
+                        Console.WriteLine(DateTime.Now.ToString("hh:MM:ss tt ") + "[Renamed] " + newDestination);
+                    }
+                    else
+                    {
+                        Console.WriteLine("[Skipping] " + file);
+                    }
+                }
+            }
+            catch(Exception err)
+            {
+                Console.WriteLine("[ERROR]: " + err.Message);
+            }
+            return result;
+        }
+
+        private void logRenamedFile(string path, string fileName)
+        {
+            File.AppendAllText(path, DateTime.Now.ToString("[yyyy-MM-dd hh:mm:ss tt] ") + fileName + Environment.NewLine);
+        }
+    }
+
+}
--- RemoveTextFromFile/Program.cs
+++ RemoveTextFromFile/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,31 @@ namespace RemoveTextFromFile
 {
     class Program
     {
+        private static FileManager fm;
+
         static void Main(string[] args)
         {
+            if(args.Length >= 2)
+            {
+                fm = new FileManager();
+
+                if(Directory.Exists(args[0]))
+                {
+                    fm.renameFile(args[0], args[1], args[2] == null ? "" : args[2]);
+                }
+                else
+                {
+                    Console.Write("Invalid path: " + args[0]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Usage:");
+                Console.WriteLine(System.AppDomain.CurrentDomain.FriendlyName + " <Source Path> <Search Criteria to Replace> (Replacement text)");
+                Console.WriteLine("");
+                Console.WriteLine("Example:");
+                Console.WriteLine(System.AppDomain.CurrentDomain.FriendlyName + " \"C:\\My Music\" \" - \"");
+            }
         }
     }
 }
